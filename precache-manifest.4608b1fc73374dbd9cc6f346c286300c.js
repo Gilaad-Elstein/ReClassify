8bba1d3a483eaa1cf600d666b1ee50fe4d6350b5
Gilaad Elstein <gilaad.elstein@gmail.com>
2020-03-06 20:43:07 +0200

    Updates

---
+++ precache-manifest.4608b1fc73374dbd9cc6f346c286300c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f9b4c4ca51a91f6749b0300391baf5f",
+    "url": "/ReClassify/index.html"
+  },
+  {
+    "revision": "a9bcb3d26bcbee3ea260",
+    "url": "/ReClassify/static/css/main.6a026c42.chunk.css"
+  },
+  {
+    "revision": "5d69d9e25ba2becfda89",
+    "url": "/ReClassify/static/js/2.da1c8f8b.chunk.js"
+  },
+  {
+    "revision": "f98a8e290e152d03115d9d57dc71c8fb",
+    "url": "/ReClassify/static/js/2.da1c8f8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9bcb3d26bcbee3ea260",
+    "url": "/ReClassify/static/js/main.b3efad20.chunk.js"
+  },
+  {
+    "revision": "4beb5f32f5d0c75a00c5",
+    "url": "/ReClassify/static/js/runtime-main.a25530ff.js"
+  }
+]);
